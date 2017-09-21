07ad86c415b907a84a7303da7f250a4db052bf8c
James Calteaux <James@Jamess-MacBook-Pro.local>
2017-09-21 09:37:15 -0500

    Initial Commit

---
+++ SteadyHands/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SteadyHands
+//
+//  Created by James Calteaux on 9/21/17.
+//  Copyright © 2017 James Calteaux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
